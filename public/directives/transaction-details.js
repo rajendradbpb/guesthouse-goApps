4c129f4c001eb8c365cc7d1c782b48cd1a28dd9a
rajashree86 <biswal.rajasree86@gmail.com>
2016-09-08 11:07:16 -0700

    updated date picker directive

--- public/directives/transaction-details.js
+++ public/directives/transaction-details.js
@@ -12,3 +12,12 @@ angular.module('guest_house').directive("transactionDetails",function(){
     //     // element.html("Student: <b>"+$scope.student.name +"</b> , Roll No: <b>"+$scope.student.rollno+"</b><br/>");
     //     element.css("background-color", "#ff00ff");
     //  }
+
+        return {
+            restrict: 'EA',
+            templateUrl: 'directives/views/datePicker.html',
+            controller:'MainController',
+             scope: false,
+            // link : linkfunction
+        };
+})
