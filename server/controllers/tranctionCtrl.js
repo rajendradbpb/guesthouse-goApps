1b435a096ad5d0aafc3812112d2183ae27a51aa2
rajendradbpb <rajendrasahoodbpb@gmail.com>
2016-09-21 03:40:25 +0530

    - updated the transaction web service

--- server/controllers/tranctionCtrl.js
+++ server/controllers/tranctionCtrl.js
@@ -10,6 +10,59 @@ var roomModelObj = require("./../models/room");
 /*
 * Tranction crud operation starts
 */
+// exports.addTranction_bk = function (req, res) {
+//     // validating gh user against the room
+//     var query = {
+//       _id: { $in: req.body.rooms },
+//       guestHouse:req.user._doc._id
+//     }
+//     roomModelObj.find(query).exec()
+//     .then(function(data) {
+//       if(data.length > 0){
+//         console.log("got the rooms");
+//         req.body.createdBy = req.body.updatedBy = req.user._doc._id;
+//         req.body.tranctionNo = req.body.tranctionNo;
+//         return tranctionModelObj(req.body).save();
+//       }
+//       else {
+//         // no room AVAILABLE for the ghuser ... send failure
+//         return res.json(response(402,"failed",constants.messages.errors.transactionfailed))
+//       }
+//
+//     })
+//     .then(function(transaction) {
+//       if(transaction)
+//       {
+//         // update the selected room status
+//         var query = {
+//           "_id": { "$in": req.body.rooms },
+//           "guestHouse":req.user._doc._id
+//         };
+//         var update = {
+//             "bookingStatus" : "CHECKED-IN"
+//         };
+//         var options = {
+//           "new" : true,
+//           "multi":true
+//         };
+//         roomModelObj.update(query,update,options).exec(function(err,updatedRooms) {
+//           if(updatedRooms){
+//             return res.json(response(200,"success",constants.messages.success.saveData,transaction));
+//           }
+//           else {
+//             return res.json(response(402,"failed",constants.messages.errors.transactionfailed));
+//           }
+//         } );
+//       }
+//       else {
+//         return res.json(response(402,"failed",constants.messages.errors.transactionfailed));
+//       }
+//     })
+//     .catch(function(err) {
+//       return res.json(response(500,"error",constants.messages.errors.saveData,err))
+//     });
+// }
+
 exports.addTranction = function (req, res) {
     // validating gh user against the room
     var query = {
@@ -17,12 +70,36 @@ exports.addTranction = function (req, res) {
       guestHouse:req.user._doc._id
     }
     roomModelObj.find(query).exec()
-    .then(function(data) {
-      console.log("2222222");
-      if(data.length > 0){
+    .then(function(rooms) {
+      if(rooms.length > 0){
         console.log("got the rooms");
         req.body.createdBy = req.body.updatedBy = req.user._doc._id;
         req.body.tranctionNo = req.body.tranctionNo;
+        // creating rooms details array
+        req.body.roomsDetails = [];
+        for(var i in rooms){
+          req.body.roomsDetails[i] = {};
+          req.body.roomsDetails[i]['room'] = rooms[i]._id;
+          req.body.roomsDetails[i]['bookingStatus'] = req.body.bookingStatus; // keeping booking status same at create transaction
+          if(req.body.checkInDate)
+          {
+            req.body.checkInDate = new Date(req.body.checkInDate);
+            req.body.roomsDetails[i]['checkInDate'] = req.body.checkInDate;
+          }
+          if(req.body.checkOutDate)
+          {
+            req.body.checkOutDate = new Date(req.body.checkOutDate);
+            req.body.roomsDetails[i]['checkOutDate'] = req.body.checkOutDate;
+          }
+          if(rooms[i].isOffer &&  rooms[i].offerPrice <  rooms[i].price) {
+            req.body.roomsDetails[i]['isOffer'] = rooms[i].isOffer;
+            req.body.roomsDetails[i]['price'] = rooms[i].offerPrice;
+          }
+          else {
+            req.body.roomsDetails[i]['price'] = rooms[i].price;
+          }
+
+        }
         return tranctionModelObj(req.body).save();
       }
       else {
--- server/models/tranction.js
+++ server/models/tranction.js
@@ -4,17 +4,33 @@ var constants = require('./../../config/constants');
 var validator = require('validator');
 var Schema = mongoose.Schema;
 var deepPopulate = require('mongoose-deep-populate')(mongoose);
+/**
+ * SchemaName :roomDetailsSchema
+ * Info : used to keep each room information of the transaction
+         type is array as each room may have differnet set of the data during check in and check out
+ *
+ * createdDate - 21-9-2016
+ * updated on -  21-9-2016 // reason for update
+ */
+var roomDetailsSchema = new mongoose.Schema({
+  room              : {type: Schema.Types.ObjectId, ref: 'room', required:constants.messages.errors.roomIdRequired },
+  checkInDate       : {type:Date , required:constants.messages.errors.dateRequired },
+  checkOutDate      : {type:Date , default:null }, // will be updated at checkOut
+  price             : {type:Number , required:constants.messages.errors.dateRequired }, // here the price may the original or the offer price
+  isOffer           : {type:Boolean , default:false  }, // checks the offer price or not
+  bookingStatus     : {type: String, enum: constants.bookingStatus, required:constants.messages.errors.bookingStatusRequired},
+})
 var tranctionSchema = new mongoose.Schema({
     otp               : {type: String, default:null },
     cName             : {type: String, required:constants.messages.errors.nameRequired },
     cMobile           : {type: String, required:constants.messages.errors.mobileRequired },
     address           : {type: String, required:constants.messages.errors.addressRequired },
-    rooms             : [{type: Schema.Types.ObjectId, ref: 'room', required:constants.messages.errors.roomIdRequired }],
+    roomsDetails      : [roomDetailsSchema],
     identity          : {type: String, required:constants.messages.errors.IDproofRequired },
     idproofno         : {type: String },
     purpose           : {type: String, default:null },
     discount          : {type: String },
-    price             : {type: Number, required:constants.messages.errors.priceRequired},// this will be updated 2nd time when the user will be check out
+    // price             : {type: Number, required:constants.messages.errors.priceRequired},// this will be updated 2nd time when the user will be check out
     isPayment         : {type: Boolean, default:false}, // this will be true in case of the checkOut
     tranctionNo       : {type:String , required:constants.messages.errors.tranctionNoRequired,unique:true},
     createdDate       : {type: Date, default: null},
