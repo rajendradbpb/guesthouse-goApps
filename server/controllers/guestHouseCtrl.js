e2a01d00675984d4f5f1e54bcdfcd9d624c0aee7
rajendradbpb <rajendrasahoodbpb@gmail.com>
2016-09-27 03:18:14 +0530

    - added price filter

--- server/controllers/guestHouseCtrl.js
+++ server/controllers/guestHouseCtrl.js
@@ -157,6 +157,16 @@ exports.getRoom = function (req, res) {
 
     var match = {};
     var aggregrate = [];
+    var filters = {};// sets the price filter flag
+    if(
+      parseInt(req.query.minPrice)
+      && parseInt(req.query.maxPrice)
+      && parseInt(req.query.minPrice)
+      && parseInt(req.query.maxPrice)  > parseInt(req.query.minPrice) ){
+      req.query.minPrice = parseInt(req.query.minPrice);
+      req.query.maxPrice = parseInt(req.query.maxPrice);
+      filters.price = true; // sets the price filter
+    }
     // req.query.checkInDate ? req.query.checkInDate = new Date(req.query.checkInDate) : new Date();
     match["$and"] = [
       {'roomsDetails.checkInDate':{'$gte':new Date(req.query.checkInDate)}},
@@ -172,25 +182,19 @@ exports.getRoom = function (req, res) {
     if(req.query.status){
       query.bookingStatus = req.query.status;
     }
-    // adding price filter
-    if(parseInt(req.query.minPrice) && parseInt(req.query.maxPrice)){
-        query.price = {
-          "$gte":parseInt(req.query.minPrice),
-          "$lte":parseInt(req.query.maxPrice)
-      }
-    }
-    // adding filter for the facility
-    if(req.query.facility && req.query.facility.split(",").length > 0 ){
-        query.facility = {
-          "$in":req.query.facility.split(",")
-      }
-    }
-    // adding filter for the room type
-    if(req.query.roomType && req.query.roomType.split(",").length > 0 ){
-        query.roomType = {
-          "$in":req.query.roomType.split(",")
-      }
-    }
+
+    // // adding filter for the facility
+    // if(req.query.facility && req.query.facility.split(",").length > 0 ){
+    //     query.facility = {
+    //       "$in":req.query.facility.split(",")
+    //   }
+    // }
+    // // adding filter for the room type
+    // if(req.query.roomType && req.query.roomType.split(",").length > 0 ){
+    //     query.roomType = {
+    //       "$in":req.query.roomType.split(",")
+    //   }
+    // }
     // validating data as per the user requested
     var select = {};
 
@@ -204,20 +208,35 @@ exports.getRoom = function (req, res) {
       // select = "name  contactDetails rooms establishDate rating MinPrice MaxPrice address";
     }
     aggregrate.push({$match:match});
-    // console.log("aggregrate   ",utility.stringify(aggregrate));
+    console.log("aggregrate   ",utility.stringify(aggregrate));
     tranctionModelObj.aggregate(aggregrate)
     .exec()
     .then(function(trans) {
-      return tranctionModelObj.populate( trans,{ "path": "roomsDetails.room" });
       // return res.json(response(200,"success",constants.messages.success.getData,trans));
+      return tranctionModelObj.populate( trans,{ "path": "roomsDetails.room" });
     })
     .then(function(trans1) {
       var nonAvailbleRoomsId = [];
       var nonAvailableRooms = [];
       for(var i in trans1){
         for(var j in trans1[i].roomsDetails){
-          nonAvailbleRoomsId.push(trans1[i].roomsDetails[j].room._id);
-          nonAvailableRooms.push(trans1[i].roomsDetails[j]);
+
+          // adding price filter
+          if(
+            filters.price
+            && trans1[i].roomsDetails[j].price >= req.query.minPrice
+            && trans1[i].roomsDetails[j].price <= req.query.maxPrice ){
+              console.log("%%%%%%%%%%%%%%%%%%%%%%%%",trans1[i].roomsDetails[j].price);
+              nonAvailbleRoomsId.push(trans1[i].roomsDetails[j].room._id);
+              nonAvailableRooms.push(trans1[i].roomsDetails[j]);
+
+          }
+          else if(!filters.price){
+            nonAvailbleRoomsId.push(trans1[i].roomsDetails[j].room._id);
+            nonAvailableRooms.push(trans1[i].roomsDetails[j]);
+          }
+          // nonAvailbleRoomsId.push(trans1[i].roomsDetails[j].room._id);
+          // nonAvailableRooms.push(trans1[i].roomsDetails[j]);
         }
       }
       // getting availble rooms
