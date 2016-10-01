18695fcead5b5bed4056d46d10c042dbb7a3be42
rajendradbpb <rajendrasahoodbpb@gmail.com>
2016-10-02 02:43:37 +0530

    - minor change

--- server/controllers/reportCtrl.js
+++ server/controllers/reportCtrl.js
@@ -14,27 +14,45 @@ var validator = require("validator");
 
 exports.getReport = function (req, res) {
   // converting to date object in server end
-  req.query.fromDate = new Date(req.query.fromDate);
-  req.query.toDate = new Date(req.query.toDate);
+  var filter= {}
+  if(req.query.fromDate &&  req.query.toDate){
+    req.query.fromDate = new Date(req.query.fromDate);
+    req.query.toDate = new Date(req.query.toDate);
+    if(validator.isDate(req.query.fromDate) && validator.isDate(req.query.toDate) && req.query.toDate >= req.query.fromDate)
+    {
+      filter.dateLimit = true;
+    }
+    else {
+      return res.status(402).json(response(402,"failed",constants.messages.errors.inValidDateLimit))
+    }
+  }
   // custom validation
-  if(validator.isNull(req.query.fromDate) || !validator.isDate(req.query.fromDate))
-    return res.status(402).json(response(402,"failed",constants.messages.errors.fromDateRequired))
-  if(validator.isNull(req.query.toDate) || !validator.isDate(req.query.toDate))
-    return res.status(402).json(response(402,"failed",constants.messages.errors.toDateRequired))
-  // validating start date and end date
-  if(req.query.fromDate > req.query.toDate)
-    return res.status(402).json(response(402,"failed",constants.messages.errors.inValidDateLimit))
+  // if(req.query.fromDate && !validator.isDate(req.query.fromDate))
+  //   return res.status(402).json(response(402,"failed",constants.messages.errors.fromDateRequired))
+  // if(req.query.toDate &&  !validator.isDate(req.query.toDate))
+  //   return res.status(402).json(response(402,"failed",constants.messages.errors.toDateRequired))
+  // // validating start date and end date
+  // if(req.query.fromDate &&  req.query.toDate && req.query.fromDate > req.query.toDate)
+  //   return res.status(402).json(response(402,"failed",constants.messages.errors.inValidDateLimit))
 
   var query = {};
-  query["$and"] = [
-    {'createdDate':{'$gte':req.query.fromDate}},
-    {'createdDate':{'$lt': utility.getDateFormat({operation:"add",mode:"day",count:1,startDate:new Date(req.query.toDate)})}}
-  ]
+  if(filter.dateLimit){
+    query["$and"] = [
+      {'createdDate':{'$gte':req.query.fromDate}},
+      {'createdDate':{'$lt': utility.getDateFormat({operation:"add",mode:"day",count:1,startDate:new Date(req.query.toDate)})}}
+    ]
+  }
   if(req.user._doc.role.type == "ghUser"){
       query.createdBy = req.user._doc._id;
   }
+  // check condition for the deepPopulate
+  var deepPopulate = 'transaction';
+  if(req.query._id){
+    deepPopulate = 'transaction.roomsDetails.room' // if the _id given we need all the details of the history
+    query._id = req.query._id;
+  }
   tranctionHistoryModelObj.find(query)
-  .deepPopulate('transaction.roomsDetails.room')
+  .deepPopulate(deepPopulate)
   .exec()
   .then(function(trans) {
     return res.json(response(200,"success",constants.messages.success.getData,trans));
